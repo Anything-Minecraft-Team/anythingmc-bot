fad58f9ca8d4cd9a6760ad19e884329436114975
JustDoom <justdoomdev@gmail.com>
2021-01-13 13:31:59 +1000

    added server jar suggestion

--- commands/serverjar.js
+++ commands/serverjar.js
@@ -6,8 +6,10 @@ module.exports = {
         .setTitle('Suggested Server Jars')
         .setDescription('Here is a suggested Server Jars to use for your server')
         .addFields(
-            {name: 'PaperMC', value: 'PaperMC is a fork of spigot that aims to improve performance '},
-            {name: 'Purpur', value: 'Purpur is a fork of '}
+            {name: 'PaperMC', value: 'PaperMC is a fork of spigot that aims to improve performance and adds tons of new features, one for example is a built in anti-xray! Its available for versions 1.8.8 to the current version, old version do not recieve updates or support. Link: https://papermc.io/'},
+            {name: 'Tuinity', value: 'Tuinity is a fork of PaperMC that increases performance even further! Its essentially early access to PaperMC features as the creator of Tuinity helps develop PaperMC on its github, he created Tuinity because it took ages for the PaperMC team to accept his changes. It is available for 1.13.2 and above. Link: https://github.com/Spottedleaf/Tuinity'},
+            {name: 'Purpur', value: 'Purpur is a fork of Tuinity (So many forks!). Its goal is to provide new and interesting configuration options, which allow for creating a unique gameplay experience not seen anywhere else! This also helps with performance. It is available for 1.14 and above. Link: https://github.com/pl3xgaming/Purpur'},
+            {name: 'Recommended Server Jar', value: 'It is very highly recommended that you use purpur instead of spigot or PaperMC, it has more features and much better performance.'}
         )
 
         message.channel.send(newEmbed);

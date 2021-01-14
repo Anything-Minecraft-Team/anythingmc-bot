c923f4b7157d85404120c8e9f28a9c20ad230b99
JustDoom <justdoomdev@gmail.com>
2021-01-14 12:24:16 +1000

    hidden node_modules i think

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-//var db = require('quick.db')
+var db = require('quick.db')
 
 client.commands = new Discord.Collection();
 

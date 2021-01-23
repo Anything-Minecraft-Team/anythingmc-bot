bc277f1d6a05ca366def5e5b7603655b7e0c83a6
JustDoom <justdoomdev@gmail.com>
2021-01-23 19:25:11 +1000

    added ratings command

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,7 @@ module.exports = {
             {name: 'Prefix', value: 'The commands prefix is /'},
             {name: '/invite', value: 'Sends the invite link for the discord bot!'},
             {name: '/review', value: 'Lets you review hosting providers through the bot, does not put the review on trustpilot or any other reviewing websites. This command is new and still in testing, if you find any bugs please contact the creator (JustDoom#1120) or make an issue on its github repo, to find it do /code. You currently cant see submitted reviews but that is being worked on currently.'},
+            {name: '/ratings', value: 'Shows all the reviews for a certain hosting provider'},
             {name: '/anticheat', value: 'Brings up a list of suggested anti-cheat plugins'},
             {name: '/permission', value: 'Brings up a suggested permission plugin'},
             {name: '/serverjar', value: 'Brings up a list of suggested server jars to use for your server'},
@@ -18,6 +19,7 @@ module.exports = {
             {name: '/badhosting, /badhosts', value: 'Brings up a list of hosting providers you should stay away from'},
             {name: '/code, /github, /repo', value: 'Brings up an embed with the github link'},
             {name: '/stats', value: 'Displays statistics about the bot'}
+            
         ).setFooter('Help keep the bot running by donating! www.paypal.com/donate?hosted_button_id=L8J9H7HTRY7L4')
 
         message.channel.send(newEmbed);
